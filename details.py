adb131394febdd14750bc62b00743a107c0e6ac1
Editxd <152770568+Editxd@users.noreply.github.com>
2023-12-17 17:56:49 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,5 +1,5 @@
 api_id = 29410389
 api_hash = "0c716764715886f6641477ffbb63e1ee"
-bot_token = "6877438713:AAEaQe_NI39L9kHan32sJ5TwA34_CPnHOzA"
+bot_token = "6739012303:AAGnV6Zn3naaeqIZJ3odvUVfc0BnBnBLY38"
 sudo_groups = -1002016605872
 
